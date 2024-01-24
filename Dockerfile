7b9599cd5b8af1a1b47f60d2fca8a64443806f78
Zulivan <>
2024-01-24 16:20:55 +0100

    Docker compose

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:18-alpine3.18
+
+ENV NODE_ENV=production
+ENV PORT=80
+
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+
+COPY package.json /usr/src/app/
+RUN npm install && npm cache clean --force
+COPY . /usr/src/app
+
+CMD [ "npm", "start" ]
+
+EXPOSE 80
---
+++ create-db.sql
@@ -0,0 +1,54 @@
+CREATE SCHEMA IF NOT EXISTS messagerie;
+
+SET SCHEMA 'messagerie';
+
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
+CREATE SEQUENCE message_seq;
+
+CREATE TABLE IF NOT EXISTS message (
+id INT DEFAULT NEXTVAL ('message_seq'),
+sender INT,
+recipient INT,
+date TIMESTAMP(0),
+content VARCHAR(255),
+isRead BOOLEAN,
+PRIMARY KEY (id)
+)  ;
+
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
+CREATE SEQUENCE user_seq;
+
+CREATE TABLE IF NOT EXISTS "user" (
+id INT DEFAULT NEXTVAL ('user_seq'),
+nickname VARCHAR(40),
+fname VARCHAR(40),
+lname VARCHAR(40),
+password VARCHAR(255),
+status VARCHAR(40),
+PRIMARY KEY (id)
+)  ;
+
+ALTER TABLE message ADD CONSTRAINT fk_message_sender FOREIGN KEY (sender) REFERENCES "user"(id);
+ALTER TABLE message ADD CONSTRAINT fk_message_recipient FOREIGN KEY (recipient) REFERENCES "user"(id);
+
+-- status
+set schema 'messagerie';
+
+CREATE SEQUENCE status_seq;
+
+CREATE TABLE IF NOT EXISTS status (
+id INT DEFAULT NEXTVAL ('status_seq'),
+name VARCHAR(40),
+color VARCHAR(40),
+PRIMARY KEY (id)
+);
+
+UPDATE messagerie.user SET status = NULL;
+
+ALTER TABLE messagerie.user ALTER COLUMN status TYPE INT USING status::integer;
+ALTER TABLE messagerie.user ADD CONSTRAINT fk_status FOREIGN KEY (status) REFERENCES status(id);
+
+INSERT INTO status VALUES (nextval('status_seq'), 'En ligne', '#00ff22');
+INSERT INTO status VALUES (nextval('status_seq'), 'Absent', '#ffa200');
+INSERT INTO status VALUES (nextval('status_seq'), 'Ne pas déranger', '#ff2e17');
+INSERT INTO status VALUES (nextval('status_seq'), 'Hors-ligne', '#000000');
