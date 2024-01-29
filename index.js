f92af85aee20f41d7976220d731862acaa8b3b7e
Zulivan <>
2024-01-29 21:05:11 +0100

    Choose SERVER_TYPES

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 import ChatServer from './chatServer/index.js'
 import APIServer from './apiServer/index.js'
 import RTCServer from './rtcServer/index.js'
+import httpServer from 'http'
 
 const http = new APIServer()
+const SERVER_TYPES = process.env.SERVER_TYPES || 'api,socket,peerjs'
 
-const socketServer = new ChatServer(http.server)
-http.defineSocket(socketServer)
+// Live chat instance
+if (SERVER_TYPES.includes('global') || SERVER_TYPES.includes('socket')) {
+  const server = httpServer.createServer(http.app)
+  server.listen(process.env.PORT || 3000)
 
-const rtcServer = new RTCServer(http.server)
-http.app.use('/rtc', rtcServer.peerServer)
+  const socketServer = new ChatServer(server)
+  http.defineSocket(socketServer)
+}
+
+// RTC Instance
+if (SERVER_TYPES.includes('global') || SERVER_TYPES.includes('peerjs')) {
+  const server2 = httpServer.createServer(http.app)
+  server2.listen(process.env.PORT_PEER || process.env.PORT || 3001)
+
+  const rtcServer = new RTCServer(server2)
+  http.app.use('/rtc', rtcServer.peerServer)
+}
