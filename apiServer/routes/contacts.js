7117c4268c912349e867fd885533346fc3a83dd9
Zulivan <>
2024-01-29 21:06:59 +0100

    Added socket relay

--- apiServer/routes/contacts.js
+++ apiServer/routes/contacts.js
@@ -5,10 +5,38 @@ import models from '../../model.js'
 // eslint-disable-next-line new-cap
 const router = express.Router({ mergeParams: true })
 
+router.get('/getPeer/:id', async (req, res) => {
+  const id = req?.params?.id
+  if (!id) return res.apiStatus(null)
+  const user = await models.User.findByPk(id, {
+    attributes: {
+      exclude: ['fname', 'lname', 'password'],
+    },
+    raw: false,
+  })
+
+  try {
+    // check if peerjsRelay is set
+    if (!user.peerjsrelay || user.peerjsrelay === '')
+      throw new Error('User is not available')
+    const peerJsRelay = user.peerjsrelay.split('|')
+
+    const output = {
+      node: peerJsRelay[0],
+      id: peerJsRelay[1],
+    }
+
+    res.apiStatus(output)
+  } catch (e) {
+    console.log(e)
+    res.apiStatus(null)
+  }
+})
+
 router.get('/getAll', async (req, res) => {
   const users = await models.User.findAll({
     attributes: {
-      exclude: ['fname', 'lname', 'password'],
+      exclude: ['fname', 'lname', 'password', 'peerjsrelay', 'socketrelay'],
     },
     raw: false,
     include: [
