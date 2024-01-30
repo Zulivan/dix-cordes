bb28de2e95c8150c09ebad00eb1a4940791eddbe
Zulivan <>
2024-01-30 14:02:24 +0100

    Refactor

--- src/store/peer.js
+++ src/store/peer.js
@@ -1,293 +1,289 @@
 import Peer from 'peerjs'
 
-export default {
-	namespaced: true,
-	state: {
-		id: '',
-		remoteId: '',
-		peer: null,
+// Initial state
+const initialState = {
+	id: '',
+	remoteId: '',
+	peer: null,
+	myLocalVideoStream: null,
+	callStatus: '',
+	remoteStreams: [],
+	receiveCalls: [],
+	usersMetadata: {},
+	receiveConnections: [],
 
-		myLocalVideoStream: null,
-		remoteStreams: [],
-		receiveCalls: [],
-		usersMetadata: {},
-		receiveConnections: [],
-		myConnections: [],
-		myCalls: [],
-	},
-	getters: {
-		getPeerId: (state) => {
-			return state.id
-		},
-		getUserMetadata: (state) => (id) => {
-			return state.usersMetadata[id]
-		},
-		getRemoteId: (state) => {
-			return state.remoteId
-		},
-		getRemoteStreams: (state) => {
-			return state.remoteStreams
-		},
-		getRemoteStreamsMetadata: (state) => {
-			return state.remoteStreams.map((item) => {
-				return state.usersMetadata[item.peer]
+	myCalls: [],
+}
+
+const getters = {
+	getPeerId: (state) => state.id,
+	getUserMetadata: (state) => (id) => state.usersMetadata[id],
+	getRemoteId: (state) => state.remoteId,
+	getRemoteStreams: (state) => state.remoteStreams,
+	getRemoteStreamsMetadata: (state) =>
+		state.remoteStreams.map((item) => state.usersMetadata[item.peer]),
+	getReceiveCalls: (state) => state.receiveCalls,
+	getReceiveConnections: (state) => state.receiveConnections,
+	getMyCalls: (state) => state.myCalls,
+	getMyLocalVideoStream: (state) => state.myLocalVideoStream,
+	getPeer: (state) => state.peer,
+	getCallStatus: (state) => state.callStatus,
+	isInCall: (state) => state.remoteStreams.length > 0,
+}
+
+const actions = {
+	async sendCallRequest({ commit, dispatch, state }, payload) {
+		commit('setCallStatus', 'Connecting to peer connection broker...')
+		await dispatch('initPeer', payload.token)
+
+		if (!state.remoteStreams.some((item) => item.peer === 'self')) {
+			state.remoteStreams.push({
+				peer: 'self',
+				mute: true,
+				stream: state.myLocalVideoStream,
 			})
-		},
-		getReceiveCalls: (state) => {
-			return state.receiveCalls
-		},
-		getReceiveConnections: (state) => {
-			return state.receiveConnections
-		},
-		getMyConnections: (state) => {
-			return state.myConnections
-		},
-		getMyCalls: (state) => {
-			return state.myCalls
-		},
-		getMyLocalVideoStream: (state) => {
-			return state.myLocalVideoStream
-		},
-		getPeer: (state) => {
-			return state.peer
-		},
-	},
-	actions: {
-		sendCallRequest: async function (context, payload) {
-			await context.dispatch('initPeer', payload.token)
+		}
 
-			// Add self to remote streams
-			if (
-				context.state.remoteStreams.filter(
-					(item) => item.peer == 'self'
-				).length == 0
-			) {
-				context.state.remoteStreams.push({
-					peer: 'self',
-					mute: true,
-					stream: context.state.myLocalVideoStream,
+		state.myCalls.push(payload.id)
+		setTimeout(() => {
+			state.peer.connect(payload.id)
+			commit('setCallStatus', 'Calling contact...')
+			state.peer.call(payload.id, state.myLocalVideoStream)
+		}, 1000)
+	},
+
+	async initPeer({ commit, state }, token) {
+		if (state.peer) {
+			state.peer.destroy()
+			state.peer = null
+		}
+
+		const payload = {
+			host: process.env.VUE_APP_PEER_SERVER_HOST,
+			port: process.env.VUE_APP_PEER_SERVER_PORT,
+			secure: process.env.VUE_APP_ENV === 'production',
+			path: '/rtc',
+			config: {
+				iceServers: [{ url: 'stun:stun1.l.google.com:19302' }],
+			},
+			debug: process.env.VUE_APP_ENV === 'production' ? 0 : 3,
+			token,
+		}
+
+		state.peer = new Peer(payload)
+		state.peer.on('open', (id) => {
+			commit('setPeerId', id)
+		})
+
+		state.peer._socket.on('message', (data) => {
+			if (data?.type == 'METADATA') {
+				commit('updateUserMetadata', {
+					id: data.id,
+					metadata: data.metadata,
 				})
+			} else if (data?.type == 'METADATAS') {
+				commit('updateUsersMetadata', data.metadata)
 			}
-			context.state.myCalls.push(payload.id)
-			setTimeout(() => {
-				context.state.peer.connect(payload.id)
-				context.state.peer.call(
-					payload.id,
-					context.state.myLocalVideoStream
-				)
-			}, 100)
-		},
-		initPeer: async function (context, token) {
-			if (context.state.peer) {
-				context.state.peer.destroy()
-				context.state.peer = null
-			}
-			const payload = {
-				host: process.env.VUE_APP_PEER_SERVER_HOST,
-				port: process.env.VUE_APP_PEER_SERVER_PORT,
-				secure: process.env.VUE_APP_ENV == 'production',
-				path: '/rtc',
-				config: {
-					iceServers: [{ url: 'stun:stun1.l.google.com:19302' }],
-				},
-				debug: process.env.VUE_APP_ENV == 'production' ? 0 : 3,
-				token: token,
-			}
-			context.state.peer = new Peer(payload)
-			context.state.peer.on('open', (id) => {
-				context.state.id = id
-			})
+		})
 
-			context.state.peer._socket.on('message', (data) => {
-				if (data?.type == 'METADATA') {
-					context.state.usersMetadata[data.id] = data.metadata
-				} else if (data?.type == 'METADATAS') {
-					context.state.usersMetadata = {
-						...context.state.usersMetadata,
-						...data.metadata,
-					}
+		state.peer._socket.on('close', (data) => {
+			console.log('socket closed', data)
+		})
+
+		state.peer.on('call', (call) => {
+			call.on('stream', (remoteStream) => {
+				const myCallIndex = state.myCalls.indexOf(call.peer)
+
+				if (myCallIndex === -1) {
+					state.myCalls.push(call.peer)
 				}
-			})
 
-			context.state.peer.on('call', (call) => {
-				call.on('stream', (remoteStream) => {
-					// Add to my calls if not already added
-					let myCallIndex = context.state.myCalls.indexOf(call.peer)
-					if (myCallIndex == -1) {
-						context.state.myCalls.push(call.peer)
-					}
-
-					let remoteStreamIndex =
-						context.state.remoteStreams.findIndex(
-							(item) => item.peer == call.peer
-						)
-					if (remoteStreamIndex == -1) {
-						context.state.peer.call(
-							call.peer,
-							context.state.myLocalVideoStream
-						)
-						context.state.remoteStreams.push({
-							peer: call.peer,
-							mute: false,
-							stream: remoteStream,
-						})
-					}
-				})
-
-				// If call peer id is in my calls, answer the call
-				let remoteStreamIndex = context.state.myCalls.findIndex(
-					(item) => item == call.peer
+				const remoteStreamIndex = state.remoteStreams.findIndex(
+					(item) => item.peer === call.peer
 				)
 
-				if (remoteStreamIndex > -1) {
-					call.answer(context.state.myLocalVideoStream)
-				} else {
-					// Check if call is already in receive calls
-					let callIndex = context.state.receiveCalls.findIndex(
-						(item) => item.peer == call.peer
-					)
-					if (callIndex != -1) return false
-
-					context.state.receiveCalls.push(call)
-				}
-			})
-
-			context.state.peer.on('connection', (connection) => {
-				connection.on('data', (data) => {
-					if (data == 'PAIR_CLOSED') {
-						context.commit('close', connection.peer)
-					}
-				})
-				connection.on('close', () => {
-					context.commit('close', connection.peer)
-				})
-
-				context.state.receiveConnections.push(connection)
-			})
-			context.state.peer.on('error', (error) => {
-				console.log(error)
-			})
-		},
-		answerCall: function (context, payload) {
-			const addLocalStreamToRemoteStreams = () => {
-				if (
-					context.state.remoteStreams.filter(
-						(item) => item.peer === 'self'
-					).length === 0
-				) {
-					context.state.remoteStreams.push({
-						peer: 'self',
-						stream: context.state.myLocalVideoStream,
+				if (remoteStreamIndex === -1) {
+					state.peer.connect(call.peer)
+					state.peer.call(call.peer, state.myLocalVideoStream)
+					state.remoteStreams.push({
+						peer: call.peer,
+						mute: false,
+						stream: remoteStream,
 					})
 				}
-			}
 
-			const handleIncomingCall = () => {
-				const lastCall =
-					context.state.receiveCalls[
-						context.state.receiveCalls.length - 1
-					]
-				lastCall.answer(context.state.myLocalVideoStream)
-				lastCall.active = true
-				context.commit('removeReceiveCall', payload)
-			}
+				commit('setCallStatus', '')
+			})
 
-			const handleExistingCall = (index) => {
-				const call = context.state.receiveCalls[index]
-				call.answer(context.state.myLocalVideoStream)
-				call.active = true
-				context.commit('removeReceiveCall', payload)
-			}
+			const remoteStreamIndex = state.myCalls.findIndex(
+				(item) => item === call.peer
+			)
 
-			addLocalStreamToRemoteStreams()
-
-			if (!payload) {
-				handleIncomingCall()
+			if (remoteStreamIndex > -1) {
+				call.answer(state.myLocalVideoStream)
 			} else {
-				let remoteStreamIndex = context.state.receiveCalls.findIndex(
-					(item) => item.peer === payload
+				const callIndex = state.receiveCalls.findIndex(
+					(item) => item.peer === call.peer
 				)
-				if (remoteStreamIndex !== -1) {
-					handleExistingCall(remoteStreamIndex)
+
+				if (callIndex === -1) {
+					state.receiveCalls.push(call)
 				}
 			}
-		},
-		rejectCall: function (context, payload) {
-			let callToReject
-			if (!payload) {
-				callToReject =
-					context.state.receiveCalls[
-						context.state.receiveCalls.length - 1
-					]
-			} else {
-				let remoteStreamIndex = context.state.receiveCalls.findIndex(
-					(item) => item.peer == payload
-				)
-				callToReject = context.state.receiveCalls[remoteStreamIndex]
+		})
+
+		state.peer.on('connection', (connection) => {
+			connection.on('data', (data) => {
+				if (data === 'PAIR_CLOSED') {
+					commit('setCallStatus', 'User ended call')
+					commit('close', connection.peer)
+				}
+			})
+
+			connection.on('close', () => {
+				console.log('connection closed by peer', connection.peer)
+				commit('close', connection.peer)
+			})
+
+			state.receiveConnections.push(connection)
+		})
+
+		state.peer.on('error', (error) => {
+			if (error.message.includes('Could not connect to peer')) {
+				commit('setCallStatus', 'User is offline')
 			}
+		})
+	},
+
+	answerCall({ commit, state }, payload) {
+		const addLocalStreamToRemoteStreams = () => {
+			if (!state.remoteStreams.some((item) => item.peer === 'self')) {
+				state.remoteStreams.push({
+					peer: 'self',
+					stream: state.myLocalVideoStream,
+				})
+			}
+		}
+
+		const handleIncomingCall = () => {
+			const lastCall = state.receiveCalls[state.receiveCalls.length - 1]
+			lastCall.answer(state.myLocalVideoStream)
+			lastCall.active = true
+			commit('removeReceiveCall', payload)
+		}
+
+		const handleExistingCall = (index) => {
+			const call = state.receiveCalls[index]
+			call.answer(state.myLocalVideoStream)
+			call.active = true
+			commit('removeReceiveCall', payload)
+		}
+
+		addLocalStreamToRemoteStreams()
+
+		if (!payload) {
+			handleIncomingCall()
+		} else {
+			const remoteStreamIndex = state.receiveCalls.findIndex(
+				(item) => item.peer === payload
+			)
+
+			if (remoteStreamIndex !== -1) {
+				handleExistingCall(remoteStreamIndex)
+			}
+		}
+	},
+
+	rejectCall({ commit, state }, payload) {
+		const callToReject = payload
+			? state.receiveCalls.find((item) => item.peer === payload)
+			: state.receiveCalls[state.receiveCalls.length - 1]
+
+		if (callToReject) {
 			callToReject.close()
-			context.commit('close', callToReject.peer)
-		},
+			commit('close', callToReject.peer)
+		}
 	},
-	mutations: {
-		setVideoStream(state, payload) {
-			state.myLocalVideoStream = payload
-		},
-		removeReceiveCall(state, payload) {
-			let index = state.receiveCalls.findIndex(
-				(item) => item.peer == payload
-			)
-			state.receiveCalls.splice(index, 1)
-		},
-		close: function (state, connectId) {
-			let callIndex = state.receiveCalls.findIndex(
-				(item) => item.peer == connectId
-			)
-			let connectionIndex = state.receiveConnections.findIndex(
-				(item) => item.peer == connectId
-			)
-			let remoteStreamIndex = state.remoteStreams.findIndex(
-				(item) => item.peer == connectId
-			)
+}
 
-			let myConnectionIndex = state.myConnections.findIndex(
-				(item) => item.peer == connectId
-			)
-			let myCallIndex = state.myCalls.findIndex(
-				(item) => item.peer == connectId
-			)
+const mutations = {
+	reset({ commit, state }) {
+		if (state.peer) {
+			state.peer.destroy()
+			state.peer = null
+		}
 
-			if (myConnectionIndex != -1)
-				state.myConnections[myConnectionIndex].send('PAIR_CLOSED')
-
-			if (callIndex != -1) state.receiveCalls[callIndex].close()
-			if (connectionIndex != -1)
-				state.receiveConnections[connectionIndex].close()
-
-			state.remoteStreams.splice(remoteStreamIndex, 1)
-			state.receiveConnections.splice(callIndex, 1)
-			state.receiveCalls.splice(connectionIndex, 1)
-
-			state.myCalls.splice(myCallIndex, 1)
-			state.myConnections.splice(myConnectionIndex, 1)
-		},
-		call: function (state, connectId) {
-			if (
-				state.myConnections.findIndex(
-					(item) => item.peer == connectId
-				) != -1
-			)
-				return false
-			if (state.myCalls.findIndex((item) => item.peer == connectId) != -1)
-				return false
-
-			let call = state.peer.call(connectId, state.myLocalVideoStream)
-
-			let dataConnection = state.peer.connect(connectId)
-
-			state.myConnections.push(dataConnection)
-			state.myCalls.push(call)
-		},
+		commit('setPeerId', '')
+		commit('setVideoStream', null)
+		commit('updateUsersMetadata', {})
 	},
-	modules: {},
+
+	setCallStatus(state, status) {
+		state.callStatus = status
+	},
+
+	setPeerId(state, id) {
+		state.id = id
+	},
+
+	setVideoStream(state, payload) {
+		state.myLocalVideoStream = payload
+	},
+
+	updateUserMetadata(state, { id, metadata }) {
+		state.usersMetadata = { ...state.usersMetadata, [id]: metadata }
+	},
+
+	updateUsersMetadata(state, metadata) {
+		state.usersMetadata = metadata
+	},
+
+	removeReceiveCall(state, payload) {
+		const index = state.receiveCalls.findIndex(
+			(item) => item.peer === payload
+		)
+		state.receiveCalls.splice(index, 1)
+	},
+
+	close(state, connectId) {
+		const callIndex = state.receiveCalls.findIndex(
+			(item) => item.peer === connectId
+		)
+		const connectionIndex = state.receiveConnections.findIndex(
+			(item) => item.peer === connectId
+		)
+		const remoteStreamIndex = state.remoteStreams.findIndex(
+			(item) => item.peer === connectId
+		)
+		const myCallIndex = state.myCalls.findIndex(
+			(item) => item.peer === connectId
+		)
+
+		if (callIndex !== -1) {
+			state.receiveCalls[callIndex].close()
+		}
+
+		if (connectionIndex !== -1) {
+			try {
+				state.receiveConnections[connectionIndex].send('PAIR_CLOSED')
+			} catch (e) {
+				console.log('closed')
+			}
+			state.receiveConnections[connectionIndex].close()
+		}
+
+		state.remoteStreams.splice(remoteStreamIndex, 1)
+		state.receiveConnections.splice(callIndex, 1)
+		state.receiveCalls.splice(connectionIndex, 1)
+
+		state.myCalls.splice(myCallIndex, 1)
+	},
+}
+
+export default {
+	namespaced: true,
+	state: { ...initialState },
+	getters,
+	actions,
+	mutations,
 }
